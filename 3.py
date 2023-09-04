af33d69b35e3e7e4bedcd71ba7e3d679fcd56f53
Thays Anchisa <thays.anchisa@gmail.com>
2023-09-04 17:52:50 -0300

    chore: chg alg 3

--- 3.py
+++ 3.py
@@ -1,4 +1,3 @@
-'''
 # entrada
 numero_um = int(input("digite o primeiro numero: "))
 numero_dois = int(input("digite o segundo numero: "))
@@ -8,11 +7,3 @@ resultado = numero_um + numero_dois
 
 #saída
 print("resultado da adição = ", resultado)
-'''
-
-numero_tres = int(input("Digite o primeiro número: "))
-numero_quatro = int(input("Digite o segundo número: "))
-
-resultado = numero_tres - numero_quatro
-
-print(f"resultado da subtração = {resultado}")
