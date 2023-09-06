af49dc3796e3d705877b6b5e69168bc7312ccfd0
Thays Anchisa <thays.anchisa@gmail.com>
2023-09-06 16:28:35 -0300

    feat: add 4.py

---
+++ 4.py
@@ -0,0 +1,13 @@
+# entrada
+numero_um = int(input("Digite o primeiro número: "))
+numero_dois = int(input("Digite o segundo número: "))
+
+#processamento
+resultado = numero_um + numero_dois
+
+#saída
+if (resultado > 10):
+    print("resultado da adição = ", resultado)
+else:
+    print("resultado da adição = ", "quase lá")
+  
