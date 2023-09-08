ede154168bbaf34d19c349779ee2f7077f2f0059
Thays Anchisa <thays.anchisa@gmail.com>
2023-09-08 10:30:25 -0300

    feat: add 8

---
+++ 8.py
@@ -0,0 +1,16 @@
+#entrada 
+#calcule 3 notas e encontre a média, 
+#se o resultado for menor que 6 "reprovado", maior que 6 "aprovado"
+
+nota1= (int(input("Nota da AV1: ")))
+nota2= (int(input("Nota da AV2: ")))
+nota3= (int(input("Nota da AV3: ")))
+
+soma=nota1+nota2+nota3
+media = soma / 3
+
+if(media >= 6):
+    resultado = "Aprovado"
+elif(media < 6):
+    resultado = "Reprovado"
+print(resultado)
