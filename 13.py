9c4d30814dd51be0f0c144575d7f0e1e6240ef49
Thays Anchisa <thays.anchisa@gmail.com>
2023-09-15 17:59:52 -0300

    feat: add 13

---
+++ 13.py
@@ -0,0 +1,12 @@
+#[laço condicional]
+#elabore um programa que efetue a entrada de um valor numero inteiro
+#processar o calculo valor da entrada x 3
+#resultado
+#execute 5x
+
+i = 1
+while (i <= 5):
+    laço= int(input("Escreva um número: "))
+    resultado = laço * 3
+    print("Resultado: ", resultado)
+    i = i + 1
